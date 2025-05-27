ed928d240e47aa6da2e122b5c1e858f8d8c191ce
David Kügler <david.kuegler@dzne.de>
2025-05-27 18:48:15 +0200

    Remove unused import to fix the code style check

--- FastSurferCNN/utils/arg_types.py
+++ FastSurferCNN/utils/arg_types.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
 from itertools import permutations, product
 from typing import Literal, cast
 
